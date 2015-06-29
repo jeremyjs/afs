2e91662dec87852ddd03f641e032a1a7402744c6
Jeremy Meyer <jjman505@gmail.com>
2015-06-28 20:53:40 -0400

    fixed GET /api response

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({ shelters: 'can access shelters' });
-  res.json({ campaigns: 'can access campaigns' });
-  res.json({ communities: 'can access communities' });
+  // TODO: put some documentation about the routes here
+  res.json({
+    shelters: 'can access shelters',
+    campaigns: 'can access campaigns',
+    communities: 'can access communities'
+  });
 });
 
 router.use('/shelters', require('./api/shelters'));
