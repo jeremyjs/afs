84bcf9bb357d989c02e88dc584028b7bf5247700
Jeremy Meyer <jjman505@gmail.com>
2015-07-16 22:20:02 -0400

    meh

--- controllers/services/ShelterService.js
+++ controllers/services/ShelterService.js
@@ -1,16 +1,22 @@
-angular.module('afs').service('shelterService', [function () {
-  this.findShelters = (query) => {
-    var Shelter = Parse.Object.extend('Shelter');
-    var query = new Parse.Query(Shelter);
-    return new Promise( (resolve, reject) => {
-      query.find({ success: resolve, error: reject });
-    });
-  }
-  this.getShelter = (id) => {
-    var Shelter = Parse.Object.extend('Shelter');
-    var query = new Parse.Query(Shelter);
-    return new Promise( (resolve, reject) => {
-      query.get(id, { success: resolve, error: reject });
-    });
-  }
-}])
+
+let test = new Shelter();
+test.title = 'my test shelter';
+test.description = 'this is a shelter';
+test.save();
+
+// angular.module('afs').service('shelterService', [function () {
+//   this.findShelters = (query) => {
+//     var Shelter = Parse.Object.extend('Shelter');
+//     var query = new Parse.Query(Shelter);
+//     return new Promise( (resolve, reject) => {
+//       query.find({ success: resolve, error: reject });
+//     });
+//   }
+//   this.getShelter = (id) => {
+//     var Shelter = Parse.Object.extend('Shelter');
+//     var query = new Parse.Query(Shelter);
+//     return new Promise( (resolve, reject) => {
+//       query.get(id, { success: resolve, error: reject });
+//     });
+//   }
+// }])
--- dev/routing.js
+++ dev/routing.js
@@ -1,25 +1,27 @@
-angular.module('afs').config(['$stateProvider', (stateProvider) => {
-  $stateProvider
-  .state('shelters', {
-    url: '/shelters',
-    resolve: {
-      shelters: ['$stateParams', 'shelterService', (params, service) => {
-        return service.findShelters();
-      }]
-    },
-    templateUrl: '../partials/shelter/list.html',
-    controller: 'sheltersController',
-    controllerAs: 'shelters'
-  })
-  .state('shelter', {
-    url: '/shelters/:id',
-    resolve: {
-      shelter: ['$stateParams', 'shelterService', (params, service) => {
-        return service.getShelter(params.id);
-      }]
-    },
-    templateUrl: '../partials/shelter/profile.html',
-    controller: 'sheltersController',
-    controllerAs: 'shelters'
-  })
-}])
+// angular.module('afs', ['ui.router']).config( ($stateProvider, $urlRouterProvider) => {
+//   $urlRouterProvider.otherwise('/shelters');
+
+//   $stateProvider
+//   .state('shelters', {
+//     url: '/shelters',
+//     resolve: {
+//       shelters: ['$stateParams', 'shelterService', (params, service) => {
+//         return service.findShelters();
+//       }]
+//     },
+//     templateUrl: '../partials/shelter/list.html',
+//     controller: 'sheltersController',
+//     controllerAs: 'shelters'
+//   })
+//   .state('shelter', {
+//     url: '/shelters/:id',
+//     resolve: {
+//       shelter: ['$stateParams', 'shelterService', (params, service) => {
+//         return service.getShelter(params.id);
+//       }]
+//     },
+//     templateUrl: '../partials/shelter/profile.html',
+//     controller: 'sheltersController',
+//     controllerAs: 'shelters'
+//   })
+// })
