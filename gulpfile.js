414be452aabe100e24e9148d12d0bf00c808bb14
Jeremy Meyer <jjman505@gmail.com>
2015-07-16 21:19:22 -0400

    added gulp-plumber and fixed teh pipes

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,37 @@ var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+
+function logError (err) {
+  // Inspect the err object
+  //console.log(err);
+
+  // Easy err reporting
+  console.log(err.toString());
+
+  // Prevent the 'watch' task from stopping
+  this.emit('end');
+}
 
 gulp.task('js:build', function() {
   return gulp.src(['dev/**/*.js', 'models/**/*.js', 'controllers/**/*.js'])
+  .pipe(plumber({ errorHandler: logError }))
   .pipe(babel())
   .pipe(concat('app.js'))
   .pipe(uglify())
   .pipe(gulp.dest('static/js'))
-  .on('error', gutil.log);
+  .on('error', logError)
 });
 
 gulp.task('less:build', function () {
   return gulp.src('dev/less/**/*.less')
+  .pipe(plumber({ errorHandler: logError }))
   .pipe(less({
     paths: [ path.join(__dirname, 'dev', 'less', 'includes') ]
   }))
   .pipe(gulp.dest('static/css'))
-  .on('error', gutil.log);
+  .on('error', logError)
 });
 
 gulp.task('default', ['less:build', 'js:build']);
