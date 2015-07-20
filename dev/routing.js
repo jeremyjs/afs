5dee66f033bfe0993fc31629514090f8eaa5e85a
Jeremy Meyer <jjman505@gmail.com>
2015-07-19 22:42:07 -0400

    added routes for shelters

--- dev/routing.js
+++ dev/routing.js
@@ -3,36 +3,29 @@ angular
 .config(['$stateProvider', '$urlRouterProvider', function StateConfig($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /campaigns
-  $urlRouterProvider.otherwise("/campaigns");
+  $urlRouterProvider.otherwise('/campaigns');
   //
   // Now set up the states
   $stateProvider
+    // Campaigns
     .state('campaigns', {
-      url: "/campaigns",
-      templateUrl: "partials/campaigns.html",
-      controller: "CampaignCtrl",
+      url: '/campaigns',
+      templateUrl: 'partials/campaigns.html',
+      controller: 'CampaignCtrl',
       controllerAs: 'vm'
     })
+
+    // Shelters
     .state('shelters', {
       url: '/shelters',
-      resolve: {
-        shelters: ['$stateParams', 'shelterService', (params, service) => {
-          return service.findShelters();
-        }]
-      },
-      templateUrl: '../partials/shelter/list.html',
-      controller: 'sheltersController',
-      controllerAs: 'shelters'
+      templateUrl: 'partials/shelter/list.html',
+      controller: 'ShelterCtrl',
+      controllerAs: 'vm'
     })
     .state('shelter', {
       url: '/shelters/:id',
-      resolve: {
-        shelter: ['$stateParams', 'shelterService', (params, service) => {
-          return service.getShelter(params.id);
-        }]
-      },
-      templateUrl: '../partials/shelter/profile.html',
-      controller: 'sheltersController',
-      controllerAs: 'shelters'
+      templateUrl: 'partials/shelter/profile.html',
+      controller: 'ShelterCtrl',
+      controllerAs: 'vm'
     })
 }]);
