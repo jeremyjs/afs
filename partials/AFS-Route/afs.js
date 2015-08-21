cb68d36dc00f7dc11c816a34f5994ac37a8b14d0
Joshua Sullivan <jsulli0527@gmail.com>
2015-08-20 22:48:40 -0700

    Routing Code
    Added More Routing to additional pages

--- partials/AFS-Route/afs.js
+++ partials/AFS-Route/afs.js
@@ -26,7 +26,8 @@ angular.module('afsLS', ['ui.router'])
 		//a fresh start
 		.state('afs.fp', {
 			url: '/fp',
-			templateUrl: 'afs-fp.html'
+			templateUrl: 'afs-fp.html',
+			controller: 'fpController'
 		});
 
 		$urlRouterProvider.otherwise('/afs/signin');
@@ -61,7 +62,8 @@ angular.module('afsLS', ['ui.router'])
 })
 
 //registration controller
-.controller("regController", function($scope) {
+.controller("regController", function($scope) 
+{
 	$scope.register= function() {
 		var _User = Parse.Object.extend("Users");
 		var query = new Parse.Query(_User);
@@ -72,9 +74,11 @@ angular.module('afsLS', ['ui.router'])
 		// put info into a database, but however before we input data
 		// we need to look for info from parse.
 		query.equalTo("username", $scope.username);
+		query.equalTo("email", $scope.email);
 		query.find({
-			success: function(results) {
-				if(results.length === 0) 
+			success: function(results) 
+			{
+				if(results.length === 0  && $scope.checkPWComb()) 
 				{
 					_user.set("username", $scope.username);
 					_user.set("password", $scope.password);
@@ -101,10 +105,47 @@ angular.module('afsLS', ['ui.router'])
 				else
 				{
 					//user already exists
-					alert("Sorry user already exists");
-					}
+					if(!$scope.checkPWComb())
+						alert("Passwords do not match");
+					else
+						alert("User already exists!");
+				}
 			},
 			error: function(error) {}
 		});
+	}
+	$scope.checkPWComb = function() {
+		if($scope.password === $scope.passwordOne) {
+			return true;
+		}
+		else
+			return false;
+	}
+})
+
+//forgotPassword controller
+
+//check to see if email exists
+//if it does send email with password reset?
+.controller('fpController', function($scope) {
+	//check email if it exists
+	//send email code
+	$scope.sendEmail = function() {
+		var _UserEmail = Parse.Object.extend("Users");
+		var query = new Parse.Query(_UserEmail);
+
+		var _userEmail = new _UserEmail();
+
+		query.equalTo("email", $scope.emailOne);
+
+		query.find({
+			success: function(results) {
+				//send email here
+				//need to make a call to the server from here
+			},
+			error: function(results, error) {
+				alert("Error: " + error.code);
+			}
+		})
 	}
 });
