55cc0920dc7331946237d7fcce58379523bc4840
Jeremy Meyer <jjman505@gmail.com>
2015-08-23 18:06:05 +0900

    fixed shelter assignments

--- controllers/ShelterCtrl.js
+++ controllers/ShelterCtrl.js
@@ -6,12 +6,10 @@ function ShelterCtrl(ShelterService) {
   // Set up vm to be the controller object. This lets us avoid scope when used with 'Controller As' syntax
   let vm = this;
   vm.test = "test";
-  vm.shelters = ShelterService.findShelters().resolve(); //(data) =>
-  //   console.log(data);
-  //   vm.shelters = data;
-  // );
-  ShelterService.getShelter().then((data) =>
-    console.log(data);
-    vm.shelter = data;
-  );
+  vm.shelters = ShelterService.findShelters().then((data) => {
+    vm.shelters = data.map((s) => { return new Shelter(s); });
+  });
+  ShelterService.getShelter().then((data) => {
+    vm.shelter = new Shelter(data);
+  });
 }
